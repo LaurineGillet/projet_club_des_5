3d162e823dead82f5584b599bbcaafc3d6920395
Laurine <laurinenet@gmail.com>
2020-02-25 14:29:35 +0100

    Header WIP

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,17 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeaderPage } from './header.page';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: HeaderPage }])
+  ],
+  declarations: [HeaderPage]
+})
+export class HeaderPageModule {}
